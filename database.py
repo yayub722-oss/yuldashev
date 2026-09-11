d6dd8ff76dc67c454c1ab20b48da1646c6776f35
Muhammadayub <yuldashevmuhammadayub577@gmail.com>
2026-09-11 18:49:56 +0500

    switch to postgresql for production

--- database.py
+++ database.py
@@ -2,11 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os 
  
- 
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///shop.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
+
 Base = declarative_base()
 
 
